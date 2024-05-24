ec24dfeb75549d9f6480ce133d68f80bfd5761e5
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-05-24 15:54:09 -0600

    build: complete data entry section

--- src/App.jsx
+++ src/App.jsx
@@ -28,31 +28,101 @@ function FormInput({
   );
 }
 
-function Form({ content, setContent }) {
-  return (
-    <form action="POST">
-      <FormInput
-        content={content}
-        setContent={setContent}
-        property="name"
-        textLabel="Name"
-      />
-      <FormInput
-        content={content}
-        setContent={setContent}
-        property="email"
-        textLabel="Email"
-        type="email"
-      />
-      <FormInput
-        content={content}
-        setContent={setContent}
-        property="phoneNumber"
-        textLabel="Phone Number"
-        type="tel"
-      ></FormInput>
-    </form>
-  );
+function Form({ display, setDisplay, content, setContent }) {
+  function handleClick() {
+    setDisplay(!display);
+  }
+
+  if (display) {
+    return (
+      <>
+        <h2>Fill out your information</h2>
+        <form action="POST">
+          <fieldset>
+            <legend>Contact info</legend>
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="name"
+              textLabel="Name"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="email"
+              textLabel="Email"
+              type="email"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="phoneNumber"
+              textLabel="Phone Number"
+              type="tel"
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Education experience</legend>
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="schoolName"
+              textLabel="School Name"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="degree"
+              textLabel="Degree Title"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="graduationDate"
+              type="date"
+              textLabel="Graduation Date"
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Most recent work experience</legend>
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="companyName"
+              textLabel="Company Name"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="jobTitle"
+              textLabel="Job Title"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="mainResponsibilities"
+              textLabel="Main Responsibilities"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="jobStartDate"
+              type="date"
+              textLabel="Start Date"
+            />
+            <FormInput
+              content={content}
+              setContent={setContent}
+              property="jobEndDate"
+              type="date"
+              textLabel="End Date"
+            />
+          </fieldset>
+          <button onClick={handleClick}>See your CV</button>
+        </form>
+      </>
+    );
+  }
 }
 
 export default function App() {
@@ -71,15 +141,17 @@ export default function App() {
   };
 
   const [formInfo, setFormInfo] = useState(initialFormFields);
-
-  function CvDisplay({ content }) {
-    return;
-  }
+  const [editorDisplayed, setEditorDisplayed] = useState(true);
 
   return (
     <>
       <h1>CV Builder</h1>
-      <Form content={formInfo} setContent={setFormInfo} />
+      <Form
+        display={editorDisplayed}
+        setDisplay={setEditorDisplayed}
+        content={formInfo}
+        setContent={setFormInfo}
+      />
     </>
   );
 }
